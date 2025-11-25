b687fcd440a15c9a65d213115d6ed050364f90e9
Sakib <saquibsaiyyed@gmail.com>
2025-11-25 20:25:07 +0530

    Even Checker Added.

--- Mycode.py
+++ Mycode.py
@@ -1,8 +1,15 @@
 
 def printNum():
+    list1 = []
     for i in range(10):
-        print(i)
+        list1.append(i)
+    return list1
 
-printNum()
-#add your evenchecker function here.
+#even checker added below
 
+def eve_checker(list1):
+    for i in list1:
+        if i%2 == 0:
+            print(i)
+
+eve_checker(printNum())
