7cbf851566296b5420ce40f31b79797a1011b043
Sakib <saquibsaiyyed@gmail.com>
2025-11-25 20:32:55 +0530

    Odd checker Added

--- Mycode.py
+++ Mycode.py
@@ -1,8 +1,15 @@
 
 def printNum():
+    list1 = []
     for i in range(10):
-        print(i)
+        list1.append(i)
+    return list1
 
-printNum()
-#add your evenchecker function here.
+#odd checker added below
 
+def odd_checker(list1):
+    for i in list1:
+        if i%2 != 0:
+            print(i)
+
+odd_checker(printNum())
